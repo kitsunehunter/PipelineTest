5a6a6e7a71fdbbcf72b0ec5f561d9f4a06e3ab97
Xavier Zhang <xzhan231@my.centennialcollege.ca>
2024-02-12 18:19:26 -0500

    initial commit

---
+++ src/main/java/com/xz/week2/DemoWeek3TestApplication.java
@@ -0,0 +1,13 @@
+package com.xz.week2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWeek3TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWeek3TestApplication.class, args);
+	}
+
+}
